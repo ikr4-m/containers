7de170560b43b97ec7bb9f92d15a6de9b3bd25cd
Ikramullah <contact@ikr4m.my.id>
2025-05-22 09:34:00 +0700

    feat(ssh): add entrypoint for respecting authorized_keys

---
+++ ssh-honeypot/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+echo "[INFO] Setup home user"
+sudo -i -u login bash <<EOF
+    set -ex
+    mkdir -p ~/.ssh
+    if [ ! -f ~/.ssh/authorized_keys ]; then
+        touch ~/.ssh/authorized_keys
+    fi
+    chmod 600 ~/.ssh/authorized_keys
+EOF
+
+echo "[INFO] Start SSH Daemon"
+/usr/sbin/sshd -D
